7ee2f115b6036323fd3d831a2a4cda86409b5866
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-11-04 16:22:43 +0530

    Navbar added

--- 1st/src/components/Nav.js
+++ 1st/src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Nav = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg" style={{ backgroundColor: '#6A1E55' }}>
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
