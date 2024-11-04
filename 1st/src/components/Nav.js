2180a61412bd386b2feec327ae42a91ddffb8749
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-11-04 16:33:33 +0530

    Navbar added

--- 1st/src/components/Nav.js
+++ 1st/src/components/Nav.js
@@ -3,40 +3,32 @@ import React from 'react'
 export const Nav = () => {
   return (
     <nav class="navbar navbar-expand-lg" style={{ backgroundColor: '#6A1E55',height:"90px" }}>
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <div class="container-fluid"  >
+      <a class="navbar-brand" href="#" style={{ color:"#FF9C73"}}>Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+        
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style={{justifyContent:"center"}} >
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <a class="nav-link active" aria-current="page" href="#" style={{ color:"#FF9C73"}}>Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <a class="nav-link" href="#" style={{ color:"#FF9C73"}}>Product</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" style={{ color:"#FF9C73"}}>Contact</a>
+          </li>
+          
         </ul>
+        </div>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
-    </div>
+  
   </nav>
   )
 }
