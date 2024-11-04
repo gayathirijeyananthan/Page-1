3311b5261964e55a31d89a08f796262d2e5b4ced
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-11-04 16:11:10 +0530

    Bootstrap added

--- 1st/src/App.js
+++ 1st/src/App.js
@@ -1,9 +1,11 @@
-
+import Nav from '../src/components/Nav';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Nav/>
     </div>
   );
 }
