4da9155a3d29c937ca3ea135f831daf76b530149
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-11-05 20:09:01 +0530

    Hero section modified

--- 1st/src/components/Hero.js
+++ 1st/src/components/Hero.js
@@ -1,18 +1,20 @@
 import React from "react";
 import '../components/css/Hero.css'
+import logo from "../assets/logo.png";
 
 
 export const Hero = () => {
   return (
     <div className="first">
-        <div className="bg"></div>
 
-      <div className="overlay"></div>
       <div>
-       <h3 className='text' >Unlock Your Potential with<br></br>  Our Learning <p style={{color:"#D91656"}}>Platform</p></h3>
+       <h3 className='text' >Unlock Your Potential with<br></br>  Our Learning Platform</h3>
+       <p className='text1' style={{color:"#D91656"}}>EduLift</p>
 
       </div>
-      <div></div>
+      <div>
+        <img className='image1' src={logo}></img>
+      </div>
     </div>
   );
 };
