9532527e172d6fb9ea3d09f76bd45713dd889705
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-11-05 19:25:06 +0530

    Hero section modified

--- 1st/src/components/Nav.js
+++ 1st/src/components/Nav.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-import '../components/css/Nav.css';
+import React from "react";
+import logo from "../assets/logo.png";
+import "../components/css/Nav.css";
 
 export const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#6A1E55', height: '90px' }}>
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#6A1E55", height: "90px" }}
+    >
       <div className="container-fluid">
         <img src={logo} alt="logo" className="navbar-logo" />
         <button
@@ -19,28 +22,48 @@ export const Nav = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{ justifyContent: 'center' }}>
+          <ul
+            className="navbar-nav me-auto mb-2 mb-lg-0"
+            style={{ justifyContent: "center" }}
+          >
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#" style={{ color: '#FF9C73' }}>
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="#"
+                style={{ color: "#FF9C73" }}
+              >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#" style={{ color: '#FF9C73' }}>
+              <a className="nav-link" href="#" style={{ color: "#FF9C73" }}>
                 Courses
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#" style={{ color: '#FF9C73' }}>
+              <a className="nav-link" href="#" style={{ color: "#FF9C73" }}>
                 Contact
               </a>
             </li>
           </ul>
         </div>
         <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn custom-outline" style={{ border: '2px solid #FF9C73', color: '#FF9C73',    cursor: 'pointer',
- }} type="submit">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn custom-outline"
+            style={{
+              border: "2px solid #FF9C73",
+              color: "#FF9C73",
+              cursor: "pointer",
+            }}
+            type="submit"
+          >
             Search
           </button>
         </form>
