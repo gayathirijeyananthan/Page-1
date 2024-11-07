3cb2d755f7b8933a0e52ab946ff7088066edfb5f
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-11-07 20:06:13 +0530

    Actual Nav start

--- 1st/src/components/Nav.js
+++ 1st/src/components/Nav.js
@@ -6,9 +6,9 @@ export const Nav = () => {
   return (
     <nav
       className="navbar navbar-expand-lg"
-      style={{ backgroundColor: "#ffff", height: "90px", position: "fixed" }}
+      style={{ backgroundColor: "#ffff", height: "90px", position: "fixed", width: "100%" }}
     >
-      <div className="container-fluid" style={{ marginTop: "40px" }}>
+      <div className="container-fluid">
         <img src={logo} alt="logo" className="navbar-logo" />
         <button
           className="navbar-toggler"
@@ -23,98 +23,73 @@ export const Nav = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul
-            className="navbar-nav me-auto mb-2 mb-lg-0"
-            style={{ justifyContent: "center", marginLeft: "500px" }}
+            className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center mx-auto"
+            style={{ listStyleType: "none", padding: "0" }}
           >
             <li className="nav-item">
               <a
                 className="nav-link active"
                 aria-current="page"
                 href="#"
-                style={{}}
               >
-                Courses{" "}
+                Courses
               </a>
             </li>
-            <span
-              class="material-symbols-outlined"
-              style={{ paddingTop: "8px" }}
-            >
-              arrow_drop_down
-            </span>
-
             <li className="nav-item">
-              <a className="nav-link active" href="#" style={{}}>
-                Teachers{" "}
+              <a className="nav-link active" href="#">
+                Teachers
               </a>
             </li>
-            <span
-              class="material-symbols-outlined"
-              style={{ paddingTop: "8px" }}
-            >
-              arrow_drop_down
-            </span>
-
             <li className="nav-item">
-              <a className="nav-link active" href="#" style={{}}>
-                Offers{" "}
+              <a className="nav-link active" href="#">
+                Offers
               </a>
             </li>
-            <span
-              class="material-symbols-outlined"
-              style={{ paddingTop: "8px" }}
-            >
-              arrow_drop_down
-            </span>
-
             <li className="nav-item">
-              <a className="nav-link active" href="#" style={{}}>
+              <a className="nav-link active" href="#">
                 Contact
               </a>
             </li>
           </ul>
         </div>
-        <form className="d-flex" role="search" style={{marginLeft:"500px"}}>
-          <div style={{marginLeft:"20px"}}>
-          <button
-            className="btn custom"
-            style={{
-              position: "relative",
-              background: "linear-gradient(to right, #6C48C5, #ED3EF7)", // Gradient for border effect
-              cursor: "pointer",
-              padding: "1px 1px",
-              borderRadius: "5px", // Optional: rounded corners
-              border: "none", // Remove default border
-              backgroundClip: "padding-box",
-              display: "inline-block",
-            }}
-            type="submit"
-          >
-            <span
+        <form className="d-flex ms-auto" role="search" style={{marginRight:"200px"}}>
+          <div className="d-flex align-items-center">
+            <button
+              className="btn custom"
               style={{
-                background: "#fff", // Button background color
-                padding: "10px 20px",
+                position: "relative",
+                background: "linear-gradient(to right, #6C48C5, #ED3EF7)", 
+                cursor: "pointer",
+                padding: "2px 2px",
+                borderRadius: "5px",
+                border: "none", 
+                backgroundClip: "padding-box",
                 display: "inline-block",
-                borderRadius: "3px", // Inner button rounding
-                fontWeight:"bold"
-
               }}
+              type="submit"
             >
-              SignIn
-            </span>
-          </button>
+              <span
+                style={{
+                  background: "#fff", 
+                  padding: "10px 20px",
+                  display: "inline-block",
+                  borderRadius: "3px", 
+                  fontWeight: "bold",
+                }}
+              >
+                SignIn
+              </span>
+            </button>
           </div>
-
           <button
-            className="btn custom"
+            className="btn custom ms-3"
             style={{
-              background: "linear-gradient(to right, #6C48C5, #ED3EF7)", // Gradient background
-              color: "white", // Set text color to white for better contrast
+              background: "linear-gradient(to right, #6C48C5, #ED3EF7)", 
+              color: "white", 
               padding: "10px 20px",
               cursor: "pointer",
               borderRadius: "5px",
-              marginLeft: "20px",
-              fontWeight:"bold"
+              fontWeight: "bold",
             }}
             type="submit"
           >
